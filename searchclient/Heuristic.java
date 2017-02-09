67b9b1a6da1e62e83269ab77cf7a6924ef22c2ef
Helge <helge@hatteland.com>
2017-02-09 19:26:45 +0100

    Pairing attempt

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -1,6 +1,7 @@
 package searchclient;
 
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.HashMap;
@@ -10,85 +11,146 @@ import java.util.Map.Entry;
 import searchclient.NotImplementedException;
 
 public abstract class Heuristic implements Comparator<Node> {
-	private final HashMap<Character,Set<Point>> goals;
+	private final HashMap<Character,HashSet<Point>> goalMap;
+	private int h, agentDistance;
+	private Point agent;
 	
 	public Heuristic(Node initialState) {
 		// Here's a chance to pre-process the static parts of the level.
-		// Map all goals to their corresponding characters
-		HashMap<Character,Set<Point>> goals = new HashMap<Character,Set<Point>>();
-		for (int row = 0; row < initialState.MAX_ROW; row++) {
-			for (int col = 0; col < initialState.MAX_COL; col++) {
-				if (initialState.goals[row][col] > 0) {
-					if (goals.containsKey(initialState.goals[row][col])) {
-						goals.get(initialState.goals[row][col]).add(new Point(row,col));
-					} else {
-						Set<Point> points = new HashSet<Point>();
-						points.add(new Point(row,col));
-						goals.put(initialState.goals[row][col],points);
-					}
-				}
+		this.goalMap = extractMap(initialState.goals);
+	}
+	
+	private HashSet<Point> findClosest(Point p1, HashSet<Point> points)
+	{
+		HashSet<Point> closest = new HashSet<Point>();
+		
+		int shortestDistance = Integer.MAX_VALUE;
+		
+		for (Point p2 : points)
+		{
+			int distance = manhattanDistance(p1, p2);
+			
+			if (distance < shortestDistance) 
+			{
+				shortestDistance = distance;
+				
+				closest.clear();
+				closest.add(p2);
+			} 
+			else if (distance == shortestDistance)
+			{
+				closest.add(p2);
 			}
 		}
-		this.goals = goals;
+		
+		return closest;
+	}
+	
+	private Point matchClosest(Point goal, HashSet<Point> goals, HashSet<Point> closestBoxes)
+	{		
+		for (Point box : closestBoxes)
+		{
+			HashSet<Point> closestGoals = findClosest(box, goals);
+			
+			if (closestGoals.contains(goal))
+			{				
+				return box;
+			}
+		}
+		return null;
+	}
+	
+	private void matchGoals(HashSet<Point> goals, HashSet<Point> boxes)
+	{		
+		HashSet<Point> unsolved = new HashSet<Point>();
+		
+		for (Point goal : goals)
+		{
+			HashSet<Point> closestBoxes = findClosest(goal, boxes);
+			
+			Point box = matchClosest(goal, goals, closestBoxes);
+			
+			if (box != null)
+			{
+				int distance = manhattanDistance(goal, box);
+				h += distance;
+				
+				//if (distance > 0)
+				//{
+				//	distance = manhattanDistance(agent, box);
+				//	if (distance < agentDistance)
+				//		agentDistance = distance;
+				//}				
+				boxes.remove(box);
+			}
+			else 
+			{				
+				unsolved.add(goal);	
+			}
+		}
+		// Compute intersection
+		goals.retainAll(unsolved);
 	}
 
 	public int h(Node n) {
-		int h = 0;
-		// Map all boxes to their corresponding characters
-		HashMap<Character,Set<Point>> boxes = new HashMap<Character,Set<Point>>();
-		for (int row = 0; row < n.MAX_ROW; row++) {
-			for (int col = 0; col < n.MAX_COL; col++) {
-				char boxChar = n.boxes[row][col];
-				// No box in cell
-				if (boxChar == 0)
-					continue;
-				// Box on goal
-				if (boxChar == Character.toUpperCase(n.goals[row][col]))
-					continue;
-				// Add box' point to boxes
-				Point boxPoint = new Point(row,col);
-				if (boxes.containsKey(boxChar)) {
-					boxes.get(boxChar).add(boxPoint);
-				} else {
-					Set<Point> points = new HashSet<Point>();
-					points.add(boxPoint);
-					boxes.put(boxChar,points);
+		h = 0;
+		agentDistance = Integer.MAX_VALUE;
+		agent = new Point(n.agentRow, n.agentCol);
+		
+		HashMap<Character,HashSet<Point>> boxMap = extractMap(n.boxes);
+		
+		for (Entry<Character,HashSet<Point>> goalEntry : goalMap.entrySet())
+		{
+			char boxChr = Character.toUpperCase(goalEntry.getKey());
+			
+			HashSet<Point> boxes = boxMap.get(boxChr);
+			HashSet<Point> goals = goalEntry.getValue();
+			
+			int matchCount = 0;
+			while (!goals.isEmpty())
+			{
+				matchGoals(goals, boxes);
+				if (++matchCount == 100)
+				{
+					System.err.println("Stuck matching");
+					break;
+				}
+			}			
+		}
+		//return h;
+		if (h == 0) return 0;
+		else return h + agentDistance;
+	}
+	
+	private int manhattanDistance(Point p1, Point p2)
+	{
+		return Math.abs(p1.x-p2.x) + Math.abs(p1.y-p2.y);
+	}
+	
+	private HashMap<Character,HashSet<Point>> extractMap(char[][] array) 
+	{
+		HashMap<Character,HashSet<Point>> map = new HashMap<Character,HashSet<Point>>();
+		
+		for (int row = 0; row < array.length; row++) 
+		{
+			for (int col = 0; col < array[0].length; col++)
+			{
+				char chr = array[row][col];
+				
+				if (chr == 0) continue;				
+				
+				if (map.containsKey(chr))
+				{
+					map.get(chr).add(new Point(row, col));
+				}
+				else
+				{
+					map.put(chr, new HashSet<Point>(Arrays.asList(new Point(row, col))));
 				}
 			}
 		}
 		
-		int agentDistance = Integer.MAX_VALUE;
-		
-		for (Entry<Character,Set<Point>> goalEntry : goals.entrySet()) {
-			char boxChar = Character.toUpperCase(goalEntry.getKey());
-			for (Point goal : goalEntry.getValue()) {
-				// Box on goal
-				if (boxChar == n.boxes[goal.x][goal.y])
-					continue;
-				// Find the box closest to the goal
-				int shortestDistance = Integer.MAX_VALUE;
-				Point shortestBox = null;
-				for (Point box : boxes.get(boxChar)) {
-					int distance = Math.abs(goal.x-box.x) + Math.abs(goal.y-box.y);
-					if (distance < shortestDistance) {
-						shortestDistance = distance;
-						shortestBox = box;
-					}
-				}
-				int distance = Math.abs(n.agentRow-shortestBox.x) + Math.abs(n.agentCol-shortestBox.y);
-				if (distance < agentDistance)
-					agentDistance = distance;
-				h += shortestDistance;
-				// Remove box to prevent goals from finding the same closest box
-				boxes.get(boxChar).remove(shortestBox);
-			}
-		}
-		
-		if (h == 0) {
-			return h;
-		}
-		
-		return h + agentDistance;
+		return map;
 	}
 
 	public abstract int f(Node n);
