545b5fdf8b41e1ee254e8bb6bab07722c22b3fda
Oliver Fleckenstein <oliverfl@live.dk>
2017-02-10 11:47:03 +0100

    Simpler huristic

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -6,12 +6,13 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.*;
 
 import searchclient.NotImplementedException;
 
 public abstract class Heuristic implements Comparator<Node> {
 	private final HashMap<Character,Set<Point>> goals;
-	
+
 	public Heuristic(Node initialState) {
 		// Here's a chance to pre-process the static parts of the level.
 		// Map all goals to their corresponding characters
@@ -34,6 +35,105 @@ public abstract class Heuristic implements Comparator<Node> {
 
 	public int h(Node n) {
 		int h = 0;
+		HashMap<Character, Set<Point>> boxes = findBoxes(n);
+		HashMap<Point, HashMap<Point, Integer>> dists = findDistances(n, boxes);
+
+		for (Entry<Point, HashMap<Point, Integer>> boxEntry : dists.entrySet()) {
+			Point closestGoal = null;
+			int minDist = Integer.MAX_VALUE;
+			for (Entry<Point, Integer> goal : boxEntry.getValue().entrySet()) {
+				if (closestGoal == null || minDist > goal.getValue()) {
+					closestGoal = goal.getKey();
+					minDist = goal.getValue();
+				}
+			}
+			h += minDist;
+		}
+		
+		if (h == 0) return 0;
+		
+		return h;
+	}
+
+	private HashMap<Point, HashMap<Point, Integer>> findDistances(Node n, HashMap<Character, Set<Point>> boxes)
+	{
+		HashMap<Point, HashMap<Point, Integer>> distances = new HashMap<Point, HashMap<Point, Integer>>();
+		
+		for (Entry<Character, Set<Point>> boxesEntrySet : boxes.entrySet()) 
+			for (Point box : boxesEntrySet.getValue())
+				distances.put(box, new HashMap<Point, Integer>());
+
+		for (Entry<Character, Set<Point>> goalEntry : goals.entrySet()) {
+			char boxChar = Character.toUpperCase(goalEntry.getKey());
+
+			// For all goals in the level
+			for (Point goal : goalEntry.getValue()) {
+				// Box on goal then ignore
+				if (boxChar == n.boxes[goal.x][goal.y])
+					continue;
+
+				int shortestDistance = Integer.MAX_VALUE;
+				Point shortestBox = null;
+
+				for (Point box : boxes.get(boxChar)) {
+					int distance = Math.abs(goal.x-box.x) + Math.abs(goal.y-box.y);
+					distances.get(box).put(goal, distance);
+				}
+			}
+		}
+
+		return distances;
+	} 
+
+	public int h_1(Node n) {
+		int h = 0;
+		HashMap<Character, Set<Point>> boxes = findBoxes(n);
+
+		int agentDistance = Integer.MAX_VALUE;
+
+		for (Entry<Character,Set<Point>> goalEntry : goals.entrySet()) {
+			char boxChar = Character.toUpperCase(goalEntry.getKey());
+			// List<Point> sortedSet = new ArrayList<Point>(goalEntry.getValue());
+			// Collections.sort(sortedSet, new Comparator<Point>() {
+			// 	public int compare(Point p1, Point p2) {
+			// 		return (p1.x - p2.x) + (p1.y - p2.y);
+			// 	}
+			// });
+
+			for (Point goal : goalEntry.getValue()) {
+				// Box on goal
+				if (boxChar == n.boxes[goal.x][goal.y])
+					continue;
+				// Find the box closest to the goal
+				int shortestDistance = Integer.MAX_VALUE;
+				Point shortestBox = null;
+
+				for (Point box : boxes.get(boxChar)) {
+					int distance = Math.abs(goal.x-box.x) + Math.abs(goal.y-box.y);
+					if (distance < shortestDistance) {
+						shortestDistance = distance;
+						shortestBox = box;
+					}
+				}
+				int distance = Math.abs(n.agentRow-shortestBox.x) + Math.abs(n.agentCol-shortestBox.y);
+				if (distance < agentDistance)
+					agentDistance = distance;
+				h += shortestDistance;
+				// Remove box to prevent goals from finding the same closest box
+				boxes.get(boxChar).remove(shortestBox);
+			}
+		}
+
+		if (h == 0) {
+			return h;
+		}
+
+		// return h;
+		return h + agentDistance;
+	}
+
+	private HashMap<Character, Set<Point>> findBoxes(Node n)
+	{
 		// Map all boxes to their corresponding characters
 		HashMap<Character,Set<Point>> boxes = new HashMap<Character,Set<Point>>();
 		for (int row = 0; row < n.MAX_ROW; row++) {
@@ -56,39 +156,7 @@ public abstract class Heuristic implements Comparator<Node> {
 				}
 			}
 		}
-		
-		int agentDistance = Integer.MAX_VALUE;
-		
-		for (Entry<Character,Set<Point>> goalEntry : goals.entrySet()) {
-			char boxChar = Character.toUpperCase(goalEntry.getKey());
-			for (Point goal : goalEntry.getValue()) {
-				// Box on goal
-				if (boxChar == n.boxes[goal.x][goal.y])
-					continue;
-				// Find the box closest to the goal
-				int shortestDistance = Integer.MAX_VALUE;
-				Point shortestBox = null;
-				for (Point box : boxes.get(boxChar)) {
-					int distance = Math.abs(goal.x-box.x) + Math.abs(goal.y-box.y);
-					if (distance < shortestDistance) {
-						shortestDistance = distance;
-						shortestBox = box;
-					}
-				}
-				int distance = Math.abs(n.agentRow-shortestBox.x) + Math.abs(n.agentCol-shortestBox.y);
-				if (distance < agentDistance)
-					agentDistance = distance;
-				h += shortestDistance;
-				// Remove box to prevent goals from finding the same closest box
-				boxes.get(boxChar).remove(shortestBox);
-			}
-		}
-		
-		if (h == 0) {
-			return h;
-		}
-		
-		return h + agentDistance;
+		return boxes;
 	}
 
 	public abstract int f(Node n);
